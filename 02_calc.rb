62917713e376d5adfb9d166628f88ee916e05bc4
feliperasan <32422863+feliperasan@users.noreply.github.com>
2023-08-22 14:04:54 +0000

    classroom 02
    classroom 02

---
+++ 02_calc.rb
@@ -0,0 +1,9 @@
+print 'Enter your first integer: '
+# .to_i convert string in integer
+number1 = gets.chomp.to_i
+print 'Enter your second integer: '
+number2 = gets.chomp.to_i
+
+addition = number1 + number2
+
+puts "The result of adding the two numbers is #{addition}"
---
+++ 02_entradadedados.rb
@@ -0,0 +1,7 @@
+print 'Type your name: '
+name = gets.chomp
+
+print 'Type your lastname: '
+lastname = gets.chomp
+
+puts "Hello #{name} #{lastname}!"
